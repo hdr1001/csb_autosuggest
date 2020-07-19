eeefdf61ee4851d04083891d78d388ee68d8262f
Hans de Rooij <hansderooij@hotmail.com>
2020-07-19 14:22:52 +0200

    Switched to Autosuggest Replaced input control with the Autosuggest and added countries array. Please note that, at this stage, code breaks as soon as the user starts typing

--- src/CountryAutosuggest.js
+++ src/CountryAutosuggest.js
@@ -1,9 +1,38 @@
 import React from "react";
+import Autosuggest from "react-autosuggest";
+const fs = require("fs");
 
 class CountryAutosuggest extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.arrCountries = null;
+    this.state = { value: "", suggestions: [] };
+
+    fs.readFile("../assets/json/isoCountry.json", (err, data) => {
+      if (err) throw err;
+      this.arrCountries = JSON.parse(data);
+      console.log("Number of countries " + this.arrCountries.length);
+    });
+  }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
   render() {
+    const { value, suggestions } = this.state;
+
+    const AutosuggestInpProps = {
+      placeholder: "Country",
+      value,
+      onChange: this.onChange
+    };
+
     return (
-      <input type="text" name="isoCountryCode" onChange={this.props.onChange} />
+      <Autosuggest suggestions={suggestions} inputProps={AutosuggestInpProps} />
     );
   }
 }
